72e7f8d10cfca508070b5476a1b5b477a231ef18
Seok-Hun <hamer1233@naver.com>
2023-11-17 15:13:07 +0900

    KSH 20231117 add module

--- post-service/src/test/java/com/example/postservice/PostServiceApplicationTests.java
+++ post-service/src/test/java/com/example/postservice/PostServiceApplicationTests.java
@@ -1,13 +1,13 @@
-package kadb.service.post;
+package com.example.postservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PostApplicationTests {
+class PostServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
